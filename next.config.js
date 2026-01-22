d9341469b661fb24bc2298efc998f7455966cc29
Geethaka123 <geethakasandesh@gmail.com>
2026-01-23 02:28:44 +0530

    Switch to static Next.js deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // appDir is now the default in Next.js 14, so we don't need to specify it
+  output: 'export',
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
